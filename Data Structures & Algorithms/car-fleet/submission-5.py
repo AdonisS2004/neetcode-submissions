3a87cd35fdbf86d3af214bc75dc97d98de29fe19
Adonis Serrano <122502284+AdonisS2004@users.noreply.github.com>
2026-06-13 16:25:56 -0400

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        def getTime(pos, vel):
+            return (target - pos) / vel
+
+        stack = []
+        cars = sorted(
+            [[float(pos), float(vel)] for pos, vel in zip(position, speed)], 
+            key=lambda x:x[0], 
+            reverse=True
+        )
+
+        for pos, vel in cars:
+            arrival_time = getTime(pos, vel)
+            if not stack or arrival_time > stack[-1]:
+                stack.append(arrival_time)
+                
+        return len(stack)
