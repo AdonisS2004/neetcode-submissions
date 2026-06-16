dc2129678f230ec24a3acaa66af4dc82f094aa9b
Adonis Serrano <122502284+AdonisS2004@users.noreply.github.com>
2026-06-16 09:43:53 -0400

    Add: trapping-rain-water - submission-12

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-12.py
@@ -0,0 +1,28 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        l, r = 0, 1
+        tall_left = []
+        tall_right = []
+        total_water = 0
+
+        # construct tallest on the left side array
+        comp = 0
+        for bar in height:
+            comp = max(comp, bar)
+            tall_left.append(comp)
+        # construct tallest on the right side array
+        comp = 0
+        for bar in height[::-1]:
+            comp = max(comp, bar)
+            tall_right.append(comp)
+        tall_right = [x for x in tall_right[::-1]]
+
+        for i, bar in enumerate(height):
+            tl, tr = tall_left[i], tall_right[i]
+            if bar < tl and bar < tr:
+                total_water += min(tl, tr) - bar
+        
+        return total_water
+            
+            
