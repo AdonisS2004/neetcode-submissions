1269f3b8bc3339c48cd13cd76d1ec5b8ce1d64fa
Adonis Serrano <122502284+AdonisS2004@users.noreply.github.com>
2026-07-14 13:23:09 -0400

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        i, size = 0, nums[0]
+        res = 0
+        while i < n-1:
+            if i + size >= n-1:
+                if i != n-1:
+                    res += 1
+                break
+            
+            next_i, next_size = i, size
+            for j in range(i+1, i+size+1):
+                # bigger window
+                if j + nums[j] > next_i + next_size:
+                    next_i, next_size = j, nums[j]
+            i, size = next_i, next_size
+            res += 1
+        return res
