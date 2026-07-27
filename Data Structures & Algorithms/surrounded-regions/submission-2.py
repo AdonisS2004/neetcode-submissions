c89e122b1a78af030e0859c4d7e7bbd6a0c87614
Adonis Serrano <122502284+AdonisS2004@users.noreply.github.com>
2026-07-26 21:36:34 -0400

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.py
@@ -0,0 +1,86 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        # idea: every cell touching an edge is safe,
+        # if the cell is connected to a safe cell, it is
+        # also safe
+
+        """
+        input
+        [
+            ["O","X","X","O","X"],
+            ["X","O","O","X","O"],
+            ["X","O","X","O","X"],
+            ["O","X","O","O","O"],
+            ["X","X","O","X","O"]
+        ]
+
+        expected
+        [
+            ["O","X","X","O","X"],
+            ["X","X","X","X","O"],
+            ["X","X","X","O","X"],
+            ["O","X","O","O","O"],
+            ["X","X","O","X","O"]
+        ]
+
+        actual
+        [
+            ["O","X","X","O","X"],
+            ["X","X","X","X","O"],
+            ["X","X","X","X","X"],
+            ["O","X","X","X","O"],
+            ["X","X","O","X","O"]
+        ]
+
+        {
+        (4, 4), (3, 4), (0, 0), 
+        (0, 3), (1, 4), (3, 0), 
+        (4, 2)}
+
+
+        """
+
+        # solution variables and functions
+        m, n = len(board), len(board[0])
+        def valid_neighbors(i, j):
+            offsets = [(1,0), (-1,0), (0,1), (0,-1)]
+            res = []
+            for di, dj in offsets:
+                if i + di >= 0 and i + di < m:
+                    if j + dj >= 0 and j + dj < n:
+                        res.append((i+di, j+dj))
+            return res
+        
+        def dfs(row, col, visited, update, condition):
+            if (row, col) in visited:
+                return
+            update(row, col, visited)
+            for nrow, ncol in valid_neighbors(row, col):
+                if condition(nrow, ncol):
+                    dfs(nrow, ncol, visited, update, condition)
+
+        # structs and helpers
+        visited = set()
+
+        def update(row, col, visited):
+            visited.add((row, col))
+        
+        def condition(row, col):
+            if board[row][col] == "O":
+                return True
+            return False
+
+        # update board
+        for r in range(m):
+            for c in range(n):
+                # Check if the cell is on any of the 4 borders
+                if (r == 0 or r == m - 1 or c == 0 or c == n - 1) and board[r][c] == "O":
+                    dfs(r, c, visited, update, condition)
+        
+        # solution
+        for r in range(m):
+            for c in range(n):
+                if (r, c) not in visited:
+                    board[r][c] = "X"
+        
+        
