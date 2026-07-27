eb239a6719c1ae8b4a315e6f9677f814a2cfbf62
Adonis Serrano <122502284+AdonisS2004@users.noreply.github.com>
2026-07-27 09:11:34 -0400

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        """
+        G is a directed graph
+        - must be acyclic
+        """
+        G = {i:[] for i in range(numCourses)}
+        for u,v in prerequisites:
+            G[u].append(v)
+        
+        # cycle detection
+        def is_acyclic_dfs(u, visited):
+            if u in visited: 
+                return False
+            visited.add(u)
+            for v in G[u]:
+                if not is_acyclic_dfs(v, visited):
+                    return False
+            visited.remove(u)
+            return True
+        
+        for u in G:
+            if not is_acyclic_dfs(u, set()):
+                return []
+        
+
+        # topological sort dfs
+        res = []
+        def toposort_dfs(u, visited):
+            if u in visited: return
+            visited.add(u)
+            for v in G[u]:
+                toposort_dfs(v, visited)
+            res.append(u)
+
+        visited = set()
+        for u in G:
+            toposort_dfs(u, visited)
+        
+        return res
