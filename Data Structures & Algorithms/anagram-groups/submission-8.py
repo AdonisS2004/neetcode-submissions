b190d2fcfe503343b95b69b6a74592dc5e2d438c
Adonis Serrano <122502284+AdonisS2004@users.noreply.github.com>
2026-08-18 11:13:35 -0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # strategy: create a hash with each string using the letters
+        anagrams = dict()
+
+        for s in strs:
+            hash = [0]*26
+            for c in s:
+                hash[ord(c)-ord('a')] += 1
+            hash = tuple(hash)
+            if hash not in anagrams:
+                anagrams[hash] = []
+            anagrams[hash].append(s)
+
+        return list(anagrams.values())
