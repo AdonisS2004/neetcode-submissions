2f3ace15a1b66c971d0cb61d2b904204fdeccbcb
Adonis Serrano <122502284+AdonisS2004@users.noreply.github.com>
2026-09-09 15:30:34 -0400

    Update: is-palindrome - submission-1

--- Data Structures & Algorithms/is-palindrome/submission-1.py
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -1,25 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        numbers = {
-            '0', '1', '2', '3','4',
-            '5', '6', '7', '8', '9'
-        }
-        letters = {
-            'a', 'b', 'c', 'd', 'e',
-            'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'o',
-            'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', 'y',
-            'z'
-        }
-        s = s.lower()
-        s_mod = []
-        for char in s:
-            if char in letters or char in numbers:
-                s_mod.append(char)
-        l, r = 0, len(s_mod)-1
-        while l < r:
-            if s_mod[l] != s_mod[r]:
+        l,r = 0, len(s)-1
+        while l<r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
