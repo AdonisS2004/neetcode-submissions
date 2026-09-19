564da8d0f157cd05bcab7fe9a485c65473e1ba94
Adonis Serrano <122502284+AdonisS2004@users.noreply.github.com>
2026-09-19 15:27:52 -0400

    Add: reorganize-string - submission-5

---
+++ Data Structures & Algorithms/reorganize-string/submission-5.py
@@ -0,0 +1,42 @@
+from collections import Counter
+import heapq
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        # 
+        freq = Counter(s)
+        n = len(s)
+        t = max(freq.values())
+        # edge case: most freq char must be less than
+
+        # if t > n//2 + 1:
+        #     return ""
+        
+        res = []
+        pq = []
+        for key, val in freq.items():
+            heapq.heappush_max(pq, (val, key))
+        while pq:
+            if len(pq) == 1:
+                freq, c = heapq.heappop_max(pq)
+                if res and (c == res[-1]):
+                    return ""
+                res.append(c)
+                freq -= 1
+                if freq != 0:
+                    heapq.heappush_max(pq, (freq, c))
+            else:
+                freqA, cA = heapq.heappop_max(pq)
+                freqB, cB = heapq.heappop_max(pq)
+                if not res or (res and res[-1] != cA):
+                    res.append(cA)
+                    res.append(cB)
+                else:
+                    res.append(cB)
+                    res.append(cA)
+                freqA -= 1
+                freqB -= 1
+                if freqA != 0:
+                    heapq.heappush_max(pq, (freqA, cA))
+                if freqB != 0:
+                    heapq.heappush_max(pq, (freqB, cB))
+        return "".join(res)
